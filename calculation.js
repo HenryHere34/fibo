16f3a32aaef072715c89f7ad56b1b450ef193749
HenryHere34 <userplx75@gmail.com>
2025-07-28 10:40:55 +0200

    description text

--- calculation.js
+++ calculation.js
@@ -3,11 +3,11 @@ document.getElementById("calcButton");
 let fibo = [0, 1, 1]
 function runCalc() {
     setInterval(() => {
-        const tempRes = Math.floor(fibo[1] + fibo[2])
-        fibo.push(tempRes)
-        fibo.shift()
+        const tempRes = Math.floor(fibo[1] + fibo[2]) // calculation
+        fibo.push(tempRes) // add result
+        fibo.shift() // remove first element
         display.textContent = fibo;
         console.log(fibo)
-    }, 1000)
+    }, 1000) // loop on mseconds
 }
 display.textContent = fibo;
